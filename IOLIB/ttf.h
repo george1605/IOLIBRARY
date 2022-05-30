a18e351a579739f6330db25ef463122789d51d2d
george1605 <32997165+george1605@users.noreply.github.com>
2022-05-30 08:46:34 +0300

    Added TTF header contents

--- IOLIB/ttf.h
+++ IOLIB/ttf.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "core.h"
+#include "file.h"
 
 namespace io
 {
+#define TTF_MAGIC 0x5F0F3CF5
 	typedef struct _ttfHeader
 	{
-
+           uint32_t version;
+           uint32_t fontRevision;
+           uint32_t checkSum;
+           uint32_t magic;
+           uint16_t flags;
 	} ttf_header;
 
 	typedef struct _ttftable
